952b3c13a52ea7fd2c61d0376b385bb08d2677c8
Maor Madai <maor.madai@campus.technion.ac.il>
2025-07-06 22:06:58 +0300

    freezing stuff

--- TransferLearning/mainFile.py
+++ TransferLearning/mainFile.py
@@ -3,12 +3,20 @@ from ultralytics import YOLO
 # from ... import functions
 newdata_path = "..." #TODO
 
-prev_model = YOLO("RoadDamageDetection/YOLOv8_Small_RDD.pt")
-model = PrepareModel(prev_model)
-model.train()
-model.val()
+model = YOLO("./../RoadDamageDetection/YOLOv8_Small_RDD.pt")
 
-def PrepareModel():
-    freezeLayers()
-    LoraLayers()
-    MoreSpicyStuff()
+
+def Change_model(model):
+    #freeze the relevant layers:
+    backbone = model.model[:9]
+    neck = model.model[10:21]
+    head = model.model[22]
+    for layer in backbone:
+        freeze_all_submodules(layer)
+    # make_lora(neck)
+    # make_lora(head)
+
+def freeze_all_submodules(layer):
+    for module in layer.modules():  # or .children() for just one level
+        for param in module.parameters():
+            param.requires_grad = False
