b3a09152933f787d64cae94cc1c1052495f63048
MaorMadai1 <maor.madai@campus.technion.ac.il>
2025-09-14 11:50:13 +0300

    update results, add final figures

--- TransferLearning/analyze.py
+++ TransferLearning/analyze.py
@@ -69,7 +69,13 @@ if __name__ == "__main__":
         if not folder_name.startswith("TRAIN-"):
             continue
 
-        configs.append(folder_name.split("batchsize=")[-1].split(",", 1)[-1])
+        # parser file name:
+        parts = folder_name.split("-")
+        epochs = next(p.split("=")[1] for p in parts if p.startswith("epochs="))
+        last_part = parts[-1].split(",")
+        lora = next(p.split("=")[1] for p in last_part if p.startswith("lora="))
+        freeze = next(p.split("=")[1] for p in last_part if p.startswith("freeze="))
+        configs.append(f"epochs: {epochs}, lora: {lora}, freeze: {freeze}")
         train_path = os.path.join(results_dir, folder_name, "weights", "last.pt")
         if not os.path.exists(train_path):
             print(f"Skipping {folder_name}, last.pt not found")
@@ -116,10 +122,10 @@ if __name__ == "__main__":
         ax.bar(x + width / 2, recall, width, label="Recall", color='salmon')
 
         ax.set_xticks(x)
-        ax.set_xticklabels(classes, rotation=45, ha='right', fontsize=7)
+        ax.set_xticklabels(classes, rotation=45, ha='right', fontsize=5)
         ax.set_ylim(0, 1)
         ax.set_ylabel("Score")
-        ax.set_title(configs[run-1])
+        ax.set_title(configs[run-1], fontsize=7)
 
     # Remove unused axes if any
     for i in range(num_runs, len(axes)):
@@ -133,6 +139,7 @@ if __name__ == "__main__":
     plt.savefig("precision_recall_per_config.png", dpi=300)
     plt.show()
 
+    # Top Classes figure
     target_classes = ["text", "fracture", "metal", "periostealreaction"]
 
     # Create figure with one subplot per class
@@ -165,11 +172,65 @@ if __name__ == "__main__":
         if i == 0:
             ax.set_ylabel("Score", fontsize=10)
         ax.grid(axis="y", linestyle="--", alpha=0.6)
+        ax.minorticks_on()
+        ax.grid(axis="y", linestyle=":", alpha=0.3, which="minor")
 
     fig.legend(["Precision", "Recall"])
 
     fig.tight_layout(rect=[0, 0, 1, 0.9])
+    plt.savefig("precision_recall_per_config_top_classes.png", dpi=300)
     plt.show()
+
+    # Top Classes best configs
+
+    target_classes = ["text", "fracture", "metal"]
+    best_configs_var = [[100, 0, None], [32, None, 10], [32, None, 9], [32, 32, 9], [32, 16, 10], [1, 0, 9]]
+    best_configs = [f"epochs: {epochs}, lora: {lora}, freeze: {freeze}" for epochs, lora, freeze in best_configs_var]
+
+    # Create figure with one subplot per class
+    fig, axes = plt.subplots(1, len(target_classes), figsize=(16, 4), sharey=True)
+
+    for i, cls in enumerate(target_classes):
+        ax = axes[i]
+
+        precisions = []
+        recalls = []
+        runs = []
+
+        for _, row in df.iterrows():
+            if cls in row["Classes"] and f"{configs[row['Run'] - 1]}" in best_configs:
+                idx = row["Classes"].index(cls)
+                precisions.append(row["Precision"][idx])
+                recalls.append(row["Recall"][idx])
+                runs.append(f"{configs[row['Run'] - 1]}")
+
+        x = np.arange(len(runs))
+        width = 0.35
+
+        bars1 = ax.bar(x - width / 2, precisions, width, label="Precision", color="skyblue")
+        bars2 = ax.bar(x + width / 2, recalls, width, label="Recall", color="salmon")
+
+        # Add labels above bars
+        ax.bar_label(bars1, fmt="%.2f", padding=2, fontsize=6)
+        ax.bar_label(bars2, fmt="%.2f", padding=2, fontsize=6)
+
+        ax.set_title(cls.capitalize(), fontsize=12)
+        ax.set_xticks(x)
+        ax.set_xticklabels(runs, rotation=45, ha="right", fontsize=8)
+        ax.set_ylim(0, 1)
+        if i == 0:
+            ax.set_ylabel("Score", fontsize=10)
+        ax.grid(axis="y", linestyle="--", alpha=0.6)
+        ax.minorticks_on()
+        ax.grid(axis="y", linestyle=":", alpha=0.3, which="minor")
+
+    fig.legend(["Precision", "Recall"])
+
+    fig.tight_layout(rect=[0, 0, 1, 0.9])
+    fig.suptitle("Best Configs, Top Classes: Precision and Recall per Class for Each Configuration", fontsize=16)
+    plt.savefig("precision_recall_per_config_top_classes_best_configs.png", dpi=300)
+    plt.show()
+
 
     # # Create a figure with 2 bar charts (avg Precision and avg Recall)
     # fig, axes = plt.subplots(1, 2, figsize=(12, 5), sharey=True)
